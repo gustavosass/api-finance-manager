cb73eb642a37c20493e27b64774840a6c525f2ea
Gustavo Sass <gustavosass@gmail.com>
2024-10-28 19:41:20 -0300

    First commit.

---
+++ src/test/java/com/gustavosass/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavosass.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
